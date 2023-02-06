ba8c2a47e99a875c50b14f3c33beb790f268103d
kowsik.periyasamy <kowsik.periyasamy@avalara.com>
2023-02-06 18:30:13 +0530

    simple update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import { Button, TextField } from '@mui/material'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export const App = () => {
+    const [allExpense, setExpense] = useState([])
+    const [amount, setAmount] = useState(0)
+
+
+    console.log(allExpense)
+    return (
+        <div>
+            <TextField label="Amount" variant="standard"
+                value={amount}
+                onChange={e => setAmount(e.target.value)}
+            />
+            <Button variant="contained"
+                onClick={() => {
+                    setExpense([...allExpense, amount])
+                    setAmount(0)
+                }}
+            >Add</Button>
+        </div >
+    )
 }
-
-export default App;
