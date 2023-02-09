4e51227f7e3c77b904f2b604403405f1bdde0c48
Kowsik <45941749+KowsikPk@users.noreply.github.com>
2023-02-09 09:45:34 +0000

    firebase config update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Button } from '@mui/material'
 import PopupForm from './components/PopupForm'
-// import ListExpense from './components/ListExpense'
+import ListExpense from './components/ListExpense'
 import Home from './Home'
+import db from './firebase.js'
+import { SocialDistance } from '@mui/icons-material'
 export const App = () => {
+    const [s, setTest] = useState([])
     const [open, setOpen] = useState(false)
     const [expenseDetailsList, setExpenseDetailsList] = useState([
         { amount: '120', category: 'Debit/Loan', paymentMode: 'Other UPI', note: 'Some ', date: 'Feb 8, 2023' }])
+
+    useEffect(() => {
+        db.collection('test').onSnapShot(snapshot => {
+            console.log(setTest(snapshot.docs.map(doc => doc)))
+            setTest(snapshot.docs.map(doc => doc.data()))
+        })
+    }, [])
     return (
         <div>
             {/* <Button variant="contained"
---
+++ src/firebase.js
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAy24PZME9omJRnRsHGwFutheFCLF1ddXw",
+  authDomain: "expense-tacker-6d9d8.firebaseapp.com",
+  projectId: "expense-tacker-6d9d8",
+  storageBucket: "expense-tacker-6d9d8.appspot.com",
+  messagingSenderId: "142606416689",
+  appId: "1:142606416689:web:b57b6e71d770c2fffd8e10",
+  measurementId: "G-Y1DPT9DVBG"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = db.firestore()
+export default db;
